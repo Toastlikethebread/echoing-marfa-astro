e6fe9328b3dcccaa9015537c075769f9c45102fe
Toast - PC <mumme.jack@gmail.com>
2025-09-25 19:36:53 +1000

    Index page completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,22 @@ import sitemap from "@astrojs/sitemap";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import icon from "astro-icon";
+
+import react from "@astrojs/react";
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+
+  integrations: [mdx(), sitemap(), icon({ 
+    include: {
+      // Include needed Icons
+      'simple-icons': ['instagram', 'tiktok']
+    }
+  }), react()],
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
